e19e632c792d1a3792610cd2ec4abe0323fc534d
Fahad Ullah Khan <fahadullahk4@gmail.com>
2024-04-29 17:32:22 +0500

    changed heart color

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var heart = document.querySelector("i");
 con.addEventListener("dblclick", () => {
 	heart.style.transform = "translate(-50%, -50%) scale(2)";
     heart.style.opacity = 0.8;
+    heart.style.color = "red";
 setTimeout(() => {
     heart.style.opacity = 0; 
 }, 1000);
